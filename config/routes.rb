2f6884926585d2be9f0e752d3b8cb7ce24fb80cf
Lincoln <antoniolincoln@live.com>
2016-08-10 16:27:19 -0300

    created controller Restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,37 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+
+  get 'restaurants/show'
+
+  get 'restaurants/new'
+
+  get 'restaurants/create'
+
+  get 'restaurants/edit'
+
+  get 'restaurants/update'
+
+  get 'restaurants/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Invent the CRUD Restaurants
+  # READ
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/:id' to: 'restaurants#show'
+
+  # CREATE
+  #get 'restaurants/new', to: 'restaurants#new'
+  #post 'restaurants', to: 'restaurants#create'
+
+  # UPDATE
+  #get 'restaurants/:id/edit' to: 'restaurants#edit'
+  #patch 'restaurants/id', to: 'restaurants#update'
+
+  # DELETE
+  #delete 'restaurants/:id' to: 'restaurants#destroy'
+
+
+
+
 end
